ebfb60d4a99dd6965f8023ba33792bf32d357f47
Neamar <neamar@neamar.fr>
2015-05-19 14:20:29 +0200

    Displaying results

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,55 @@
-var ViewModel = function(token) {
-  this.token = ko.observable(token);
+var ResultModel = function(data) {
+  var self = this;
 
-  this.searchQuery = ko.observable('');
+  Object.keys(data).forEach(function(key) {
+    self[key] = ko.observable(data[key]);
+  });
+};
+
+var ViewModel = function(token) {
+  var self = this;
+
+  // Current token, used to communicate with Anyfetch
+  self.token = ko.observable(token.toString());
+
+  // Current search query (simple string)
+  // binded to text inputs
+  self.searchQuery = ko.observable('tariq krim');
+
+  // Raw results from the API (not an observable, not on self)
+  var rawResults = null;
+
+  // Real results
+  self.results = ko.observableArray([]);
+
+  // Search for results
+  self.doSearch = function doSearch() {
+    console.log("Searching", self.searchQuery());
+    $.ajax("https://api.anyfetch.com/documents",
+    {
+      data: {
+        search: self.searchQuery(),
+        render_templates: true,
+      },
+      headers: {
+        authorization: 'Bearer ' + self.token()
+      },
+      success: function(data, status) {
+        rawResults = data;
+
+        var results = data.data.map(function(rawResult) {
+          return new ResultModel(rawResult);
+        });
+
+        console.log(results);
+        self.results(results);
+      },
+      error: function(xhr, status, error) {
+        console.error(status, error);
+        // TODO: handle error.
+      }
+    });
+  };
 };
 
 var currentToken = Cookies.get("token");
